2e95acf9d79c14bdfd6c6dd554151648fcc52ea5
Anderson Santos <dev.anderson.santos@gmail.com>
2018-08-09 23:58:21 -0300

    Configuração do caminho das páginas com a classe Page

--- index.php
+++ index.php
@@ -2,17 +2,17 @@
 
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+use Codes\Page;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
+	$page = new Page();
 
-	$sql = new Codes\DB\Sql();
-
-	$results = $sql->select("SELECT * FROM tb_users");
-
-	echo json_encode($results);
+	$page->setTpl("index");
 
 });
 
